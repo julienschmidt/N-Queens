d461872b5d993aa2ca9bb3048196d60bbc3cbcf8
Julien Schmidt <github@julienschmidt.com>
2013-01-31 00:53:28 +0100

    Final version
    The ipmortant improvment is the new "smart" loop on line 118 instead of
    the old "try-all" loop. Now we preselect positions, where placing a
    Queen might be possible.
    On my system this cut runtime from 2,6s to only 0,7s for n=15

--- NQueens.java
+++ NQueens.java
@@ -58,83 +58,85 @@ public class NQueens {
 
     /**
      * Calculates number of possible solutions for n queens on n*n sized board
-     *
+     * 
      * @param n queens count
      * @return  solutions count
      */
     private static int solveNQueens(int n) {
-        //reCount++;
-        int sol = 0;
-        int med = n >> 1; // n/2
+        int sol     = 0; 
+        int med     = n >> 1;       // n/2
+        int nMask   = (1 << n) - 1; // n-long bitmask filled with 1s
         int col;
 
         // For every distinct solution exists another symmetric solution
         // mirrored around the Y-axis (middle).
-        // So it is still possible to get all solutions by calculating only
+        // So it is still possible to get all solutions by calculating only 
         // combinations with the first queen placed on the left half.
         for (int y = 0; y < med; y++) {
             col = 1 << y; // set bit on index y
-
+            
             // Count solutions twice (solution + mirrored solution).
-            // col >>> 1 | shift col values 1 index to right and discards
+            // col >>> 1 : shift col values 1 index to right and discards
             //      remainder (unsigned right shift).
-            // col << 1 | shift col values 1 index to left.
-            sol += 2 * findPos(1, n, col, col >>> 1, col << 1);
-        }
+            // col << 1  : shift col values 1 index to left.
+            sol += (findPos(nMask, col, col >>> 1, col << 1) << 1);
+        }    
 
         // If n is odd, the middle column must be treaded separately, because
-        // the solution mirrored around the Y-axis is the same es the solution
-        // itself
-        if (1 == ((n) & 1)) { // (n) & 1 <=> n%2, 1 if n is odd
-            col = 1 << med; // set bit on index med
-
-            // col >>> 1 | shift col values 1 index to right and discards
+        // no mirrored solution exists
+        if (1 == ((n) & 1)) {   // (n) & 1 <=> n%2, 1 if n is odd
+            col = 1 << med;     // set bit on index med
+            
+            // col >>> 1 : shift col values 1 index to right and discards
             //      remainder (unsigendes right shift).
-            // col << 1 | shift col values 1 index to left.
-            sol += findPos(1, n, col, col >>> 1, col << 1);
+            // col << 1  : shift col values 1 index to left.
+            sol += findPos(nMask, col, col >>> 1, col << 1);
         }
-
+        
         return sol;
     }
-
+    
     /**
-     * Rekursive method to find all possible places for the next queen
-     *
-     * @param max   Total queens count
-     * @param num   Count of queens already placed
-     * @param bmCol BitMask for columns with existing queens
-     * @param bmDL  BitMask for diagonals from (top) left with existing queens
-     * @param bmDR  BitMask for diagonals from (top) right with existing queens
+     * Recursive method to find all possible places for the next queen
+     * 
+     * @param nMask bitmask filled with 1s and n-length
+     * @param bmRow bitmask for rows with existing queens
+     * @param bmDL  bitmask for diagonals from (top) left with existing queens
+     * @param bmDR  bitmask for diagonals from (top) right with existing queens
      * @return      number of solutions found
      */
-    private static int findPos(int num, int max, int bmCol, int bmDL, int bmDR) {
+    private static int findPos(int nMask, int bmRow, int bmDL, int bmDR) {    
+        //reCount++;
         int sol = 0;
-        int mask = bmCol | bmDL | bmDR;
-        int y, col;
-
-        // Try all y-positions
-        for (y = 0; y < max; y++) {
-            col = 1 << y;
-
-            // Queen can't be placed in the same column or a diagonal with an
-            // existing queen
-            if(0 != (col & mask))
-                continue;
-
-            // Check if there are no more queens to be placed
-            if (num+1 == max) {
-                // No more queens, increment solutions count
-                sol++;
-            }  else {
+        int y, newBmRow;
+        
+        // bitmask with 1s on index of y-positions where placing a queen is
+        // possible
+        int yMask = nMask & ~(bmRow | bmDL | bmDR);
+        
+        // Branch all possible y-positions
+        while(yMask > 0) {
+            y           = -yMask & yMask;   // Y-position
+            newBmRow    = bmRow | y;        // new row-bitmask
+            
+            // Remove this Y-position from the yMask for the next run
+            yMask ^= y;
+            
+            // Check if there are more queens to be placed
+            if (newBmRow < nMask) {
                 // More queens must be placed, make a recursive call for the
                 // next queen
                 sol += findPos(
-                    num+1,
-                    max,
-                    bmCol | col,
-                    (bmDL | col) >>> 1,
-                    (bmDR | col) << 1
+                    nMask,
+                    newBmRow,
+                    (bmDL | y) >>> 1,
+                    (bmDR | y) << 1
                 );
+                continue;
+            
+            } else {
+                // No more queens, increment solutions count
+                sol++;
             }
         }
 
