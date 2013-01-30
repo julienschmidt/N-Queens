1e66dac97502938eb4def149d64cedca3680332c
Julien Schmidt <github@julienschmidt.com>
2013-01-31 00:45:57 +0100

    Rewrite with bitwise operations
    HUGE impact on performance.
    On my system this cut runtime for n=15 from ~17s to only 2,6s

--- NQueens.java
+++ NQueens.java
@@ -2,7 +2,6 @@
  */
 package nqueens;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -46,74 +45,96 @@ public class NQueens {
 
         // START MEASURING
         start = System.currentTimeMillis();
-        solutions = solve(n, board);
+        solutions = solveNQueens(n);
         end = System.currentTimeMillis();
         // END MEASURING
 
         // Print result
-        System.out.println("" + n + "-Queens: Found " + solutions + " Solutions in " + (end-start) + "ms!");
-        //System.out.println("" + n + "-Queens: Found " + solutions + " Solutions in " + (end-start) + "ms! ["+reCount+" executions]");
+        System.out.println("" + n + "-Queens: Found " +
+            solutions + " Solutions in " + (end - start) + "ms!");
+        //System.out.println("" + n + "-Queens: Found " + solutions +
+        //  " Solutions in " + (end-start) + "ms! ["+reCount+" executions]");
     }
 
-    public static int solve(int i, int[] board) {
+    /**
+     * Calculates number of possible solutions for n queens on n*n sized board
+     *
+     * @param n queens count
+     * @return  solutions count
+     */
+    private static int solveNQueens(int n) {
         //reCount++;
-        int max = board.length; // faster as field
         int sol = 0;
-        //int y, x, col;
+        int med = n >> 1; // n/2
+        int col;
 
+        // For every distinct solution exists another symmetric solution
+        // mirrored around the Y-axis (middle).
+        // So it is still possible to get all solutions by calculating only
+        // combinations with the first queen placed on the left half.
+        for (int y = 0; y < med; y++) {
+            col = 1 << y; // set bit on index y
 
-        if(i == max) {
-            int odd     = (max) &1; // max%2
-            int half    = max>>1;   // max/2
+            // Count solutions twice (solution + mirrored solution).
+            // col >>> 1 | shift col values 1 index to right and discards
+            //      remainder (unsigned right shift).
+            // col << 1 | shift col values 1 index to left.
+            sol += 2 * findPos(1, n, col, col >>> 1, col << 1);
+        }
 
-            // For every distinct solution exists another symmetric solution
-            // mirrored around the Y-axis (middle).
-            // So it is still possible to get all solutions by calculating only
-            // combinations with the first queen placed on the left half.
-            for(int y=0; y < half; y++) {
-                board[0] = y;
-                //System.out.println("1 " + y);
-                sol += 2*solve(i-1, board.clone());
-            }
+        // If n is odd, the middle column must be treaded separately, because
+        // the solution mirrored around the Y-axis is the same es the solution
+        // itself
+        if (1 == ((n) & 1)) { // (n) & 1 <=> n%2, 1 if n is odd
+            col = 1 << med; // set bit on index med
 
-            // If n is odd, the middle column must be treaded separately, because
-            // no mirrored solution exists
-            if(1 == odd) {
-                board[0] = half;
-                //System.out.println("h " + half);
-                sol += solve(i-1, board);
-            }
-        } else {
-            int y, x, col;
-            int num = max - i;
+            // col >>> 1 | shift col values 1 index to right and discards
+            //      remainder (unsigendes right shift).
+            // col << 1 | shift col values 1 index to left.
+            sol += findPos(1, n, col, col >>> 1, col << 1);
+        }
 
-            out: for(y=0; y < max; y++) {
-                // Check for all existing Queens if they threaten the current
-                // positions
-                for(x=0; x < num; x++) {
-                    col = board[x];
-                    // same pos || num-x == abs(y-col) -> diagonal
-                    if(col == y || num-x == y-col || num-x == -(y-col))
-                        continue out;
-                }
+        return sol;
+    }
 
-                if(i == 1) {
-                    sol++;
-                } else {
-                    board[num] = y;
-                    sol += solve(i-1, board.clone());
-                }
-                /* Slower :(
-                } else if(y == max-1) {
-                    board[num] = y;
-                    sol += solve(i-1, board);
-                } else {
-                    int[] newBoard = new int[max];
-                    System.arraycopy(board, 0, newBoard, 0, max);
-                    //int[] newBoard = (y == max-1) ? board: board.clone();
-                    newBoard[num] = y;
-                    sol += solve(i-1, newBoard);
-                }*/
+    /**
+     * Rekursive method to find all possible places for the next queen
+     *
+     * @param max   Total queens count
+     * @param num   Count of queens already placed
+     * @param bmCol BitMask for columns with existing queens
+     * @param bmDL  BitMask for diagonals from (top) left with existing queens
+     * @param bmDR  BitMask for diagonals from (top) right with existing queens
+     * @return      number of solutions found
+     */
+    private static int findPos(int num, int max, int bmCol, int bmDL, int bmDR) {
+        int sol = 0;
+        int mask = bmCol | bmDL | bmDR;
+        int y, col;
+
+        // Try all y-positions
+        for (y = 0; y < max; y++) {
+            col = 1 << y;
+
+            // Queen can't be placed in the same column or a diagonal with an
+            // existing queen
+            if(0 != (col & mask))
+                continue;
+
+            // Check if there are no more queens to be placed
+            if (num+1 == max) {
+                // No more queens, increment solutions count
+                sol++;
+            }  else {
+                // More queens must be placed, make a recursive call for the
+                // next queen
+                sol += findPos(
+                    num+1,
+                    max,
+                    bmCol | col,
+                    (bmDL | col) >>> 1,
+                    (bmDR | col) << 1
+                );
             }
         }
 
